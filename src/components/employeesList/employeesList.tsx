d266f78c31aec3bd0e7b3e93a004a11d43d9f9c8
Oleh Taran <podadi2003@gmail.com>
2023-08-27 17:57:08 +0300

    add ids for card list, need to check styles

--- src/components/employeesList/employeesList.tsx
+++ src/components/employeesList/employeesList.tsx
@@ -4,25 +4,29 @@ const employees = [
   {
     name: "Anna",
     img: "/employee.jpg",
-    description: "Hey there! I'm Anna, and I absolutely adore working as a guide on boats"
+    description: "Hey there! I'm Anna, and I absolutely adore working as a guide on boats",
+    id: 0
   },
   {
     name: "Maxim",
     img: "/employee.jpg",
-    description: "I feel free and strong. I love teaching our clients how to navigate the boat, sharing my expertise."
+    description: "I feel free and strong. I love teaching our clients how to navigate the boat, sharing my expertise.",
+    id: 1
   },
   {
     name: "Elena",
     img: "/employee.jpg",
-    description: "My job is to teach our clients to enjoy the sea, feel its rhythm and harmony. It's just an incredible feeling!"
+    description: "My job is to teach our clients to enjoy the sea, feel its rhythm and harmony. It's just an incredible feeling!",
+    id: 2
   },
   {
     name: "Ivan",
     img: "/employee.jpg",
-    description: "Hello! I'm Ivan, the mechanic and technical specialist. Sea adventures are my calling. I ensure that our boats are always in perfect condition"
+    description: "Hello! I'm Ivan, the mechanic and technical specialist. Sea adventures are my calling. I ensure that our boats are always in perfect condition",
+    id: 3
   }
 ];
 
 export const EmployeesList = () => {
-  return <div className={styles.employeesList}>{employees.map(employee => <EmployeeCard name={employee.name} img={employee.img} description={employee.description} />)}</div>
+  return <div className={styles.employeesList}>{employees.map(employee => <EmployeeCard key={employee.id} name={employee.name} img={employee.img} description={employee.description} />)}</div>
 }
