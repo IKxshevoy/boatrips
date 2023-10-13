ea444bba59430e37b6432381bbfbba03dd0df75b
Ivan Koshevoy <104228541+IKxshevoy@users.noreply.github.com>
2023-10-13 21:38:53 +0100

    rebuilt tours page, removed animations (#23)

--- src/components/aboutUs/aboutUsMap/AboutUsMap.tsx
+++ src/components/aboutUs/aboutUsMap/AboutUsMap.tsx
@@ -1,20 +1,16 @@
 import { Title } from "@/ui-kit/title/Title";
-import Fade from "react-reveal/Fade";
 import styles from "@/components/aboutUs/aboutUs.module.scss";
 
 export const AboutUsMap = () => (
   <div
     className={`${styles.about__container} ${styles.about__container__directionColumnMap}`}
   >
-    <Fade top>
       <div className={styles.about__data}>
         <Title title="How to find us ?" />
         <p className={styles.about__description}>
           Look for a yellow stand with the name "Boatrips" in Marina de Lagos.
         </p>
       </div>
-    </Fade>
-    <Fade bottom>
       <iframe
         src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3181.8740420200984!2d-8.674127499999999!3d37.1081157!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd1b317c8dca2f53%3A0xcdb8b3e009b5324e!2sBoatrips-Boat%20Trips%20and%20Tours!5e0!3m2!1sru!2sua!4v1693492808564!5m2!1sru!2sua"
         width="100%"
@@ -24,6 +20,5 @@ export const AboutUsMap = () => (
         loading="lazy"
         referrerPolicy="no-referrer-when-downgrade"
       ></iframe>
-    </Fade>
   </div>
 );
