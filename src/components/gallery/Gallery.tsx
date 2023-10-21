7aa37aa59d65dee0fe983e13546610163272e6eb
Ivan Koshevoy <ivan.koshevoyyy@gmail.com>
2023-10-21 18:46:18 +0100

    gallery update

--- src/components/gallery/Gallery.tsx
+++ src/components/gallery/Gallery.tsx
@@ -14,24 +14,6 @@ const Gallery = () => {
         </h2>
         <ul className={styles.imageGallery}>
           <li>
-            <img src="/gallery/1.jpg" alt="" />
-            <div className={styles.overlay}>
-              <span>Benagil Caves</span>
-            </div>
-          </li>
-          <li>
-            <img src="/gallery/2.jpg" alt="" />
-            <div className={styles.overlay}>
-              <span>Coastline Tour</span>
-            </div>
-          </li>
-          <li>
-            <img src="/gallery/3.jpg" alt="" />
-            <div className={styles.overlay}>
-              <span>Ponta de Piedade</span>
-            </div>
-          </li>
-          <li>
             <img src="/gallery/4.jpg" alt="" />
             <div className={styles.overlay}>
               <span>Ponta de Piedade</span>
@@ -51,30 +33,6 @@ const Gallery = () => {
           </li>
           <li>
             <img src="/gallery/7.jpg" alt="" />
-            <div className={styles.overlay}>
-              <span>Image title</span>
-            </div>
-          </li>
-          <li>
-            <img src="/gallery/8.jpg" alt="" />
-            <div className={styles.overlay}>
-              <span>Image title</span>
-            </div>
-          </li>
-          <li>
-            <img src="/gallery/15.jpg" alt="" />
-            <div className={styles.overlay}>
-              <span>Image title</span>
-            </div>
-          </li>
-          <li>
-            <img src="/gallery/16.jpg" alt="" />
-            <div className={styles.overlay}>
-              <span>Image title</span>
-            </div>
-          </li>
-          <li>
-            <img src="/gallery/18.jpg" alt="" />
             <div className={styles.overlay}>
               <span>Image title</span>
             </div>
